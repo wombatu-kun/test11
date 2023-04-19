9318f26b65284b513b9e92fc32edafcb02354857
Vova Kolmakov <vlvkolmakov@nsk.beeline.ru>
2023-04-19 17:05:59 +0700

    fixed RequestRateLimiter

--- gatewayserver/src/main/java/wombatukun/tests/test11/gateway/GatewayApp.java
+++ gatewayserver/src/main/java/wombatukun/tests/test11/gateway/GatewayApp.java
@@ -5,8 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 import wombatukun.tests.test11.common.security.JWTUtil;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Optional;
 
 @SpringBootApplication
 @RefreshScope
@@ -23,6 +29,15 @@ public class GatewayApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApp.class, args);
+	}
+
+	@Bean
+	public KeyResolver userKeyResolver() {
+		return exchange -> Optional.ofNullable(exchange.getRequest().getRemoteAddress())
+				.map(InetSocketAddress::getAddress)
+				.map(InetAddress::getHostAddress)
+				.map(Mono::just)
+				.orElse(Mono.empty());
 	}
 
 }
