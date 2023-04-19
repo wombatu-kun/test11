79ed7237487edfc18064332e697af98a8f75faa5
Vova Kolmakov <vlvkolmakov@nsk.beeline.ru>
2023-04-19 15:14:11 +0700

    remove delay

--- auth-service/src/main/java/wombatukun/tests/test11/authservice/controllers/UserController.java
+++ auth-service/src/main/java/wombatukun/tests/test11/authservice/controllers/UserController.java
@@ -22,7 +22,6 @@ import wombatukun.tests.test11.common.security.Role;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -50,11 +49,6 @@ public class UserController extends GlobalExceptionHandler {
         Map<String, Object> userInfo = new HashMap<>();
         userInfo.put("user", authentication.getPrincipal());
         userInfo.put("authorities", AuthorityUtils.authorityListToSet(authentication.getAuthorities()));
-        try {
-            TimeUnit.SECONDS.sleep(4);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         return CommonResponse.success(userInfo);
     }
 
